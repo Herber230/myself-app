e57bdeb7efcf8bee3aab9b29ddcedba3ed68d671
Herber Colop <herber16ext@gmail.com>
2023-03-06 23:36:00 -0600

    feat: repo setup - lint test and git hooks (#1)

---
+++ jest.config.ts
@@ -0,0 +1,49 @@
+const config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/mocks/**',
+  ],
+  coveragePathIgnorePatterns: [],
+  setupFilesAfterEnv: ['./test/setup-tests.ts'],
+  testEnvironment: 'jsdom',
+  modulePaths: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.(ts|js|tsx|jsx)$': '@swc/jest',
+  },
+  transformIgnorePatterns: [
+    '[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  moduleFileExtensions: [
+    // Place tsx and ts to beginning as suggestion from Jest team
+    // https://jestjs.io/docs/configuration#modulefileextensions-arraystring
+    'tsx',
+    'ts',
+    'web.js',
+    'js',
+    'web.ts',
+    'web.tsx',
+    'json',
+    'web.jsx',
+    'jsx',
+    'node',
+  ],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
+  resetMocks: true,
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
+};
+
+export default config;
